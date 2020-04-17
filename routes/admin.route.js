bd2df67a081191ae0d7e1a59c5b7e28320f9717c
= <=>
2020-04-17 11:33:30 +0700

    modified intersection route

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -7,22 +7,22 @@ var intersectionControllers = require('../controllers/intersection.controller');
 
 var intersectionValidate = require('../validates/intersection.validate');
 
-router.post('/user/create-user', userControllers.createUser);
-router.put('/user/block-user/:id', userControllers.blockedUser);
-router.put('/user/unlock-user/:id', userControllers.unlockedUser);
-router.delete('/user/delete-user/:id', userControllers.deleteUser);
-router.get('/user/get-all', userControllers.getAllUsers);
-router.get('/user/get/:id', userControllers.getUser);
+router.post('/user', userControllers.createUser);
+router.put('/user/blocked/:id', userControllers.blockedUser);
+router.put('/user/unlocked/:id', userControllers.unlockedUser);
+router.delete('/user/:id', userControllers.deleteUser);
+router.get('/user', userControllers.getAllUsers);
+router.get('/user/:id', userControllers.getUser);
 
-router.post('/device/', deviceControllers.createDevice);
+router.post('/device', deviceControllers.createDevice);
 router.get('/device/', deviceControllers.getAllDevices);
 router.put('/device/blocked/:id', deviceControllers.blockedDevice);
 router.put('/device/unblocked/:id', deviceControllers.unBlockedDevice);
 router.delete('/device/:id', deviceControllers.deleteDevice);
 
-router.post('/intersection', intersectionValidate.checkField, intersectionValidate.nameExists, intersectionControllers.createIntersection);
+router.post('/intersection', intersectionValidate.nameExists, intersectionControllers.createIntersection);
 router.delete('/intersection/:id', intersectionControllers.deleteIntersection);
-router.put('/intersection/:id', intersectionValidate.checkField, intersectionControllers.editIntersection);
+router.put('/intersection/:id', intersectionControllers.editIntersection);
 router.put('/intersection/config-time/:id', intersectionControllers.configTime);
 
 
--- routes/intersection.route.js
+++ routes/intersection.route.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 var intersectionControllers = require('../controllers/intersection.controller')
 
-router.get('/get-all', intersectionControllers.getAllIntersections);
-router.get('/get/:id', intersectionControllers.getIntersection);
-router.put('/mode-control/manual/:id', intersectionControllers.updateManualControl);
-router.put('/mode-control/automatic/:id', intersectionControllers.updateAutomaticControl);
+router.get('/', intersectionControllers.getAllIntersections);
+router.get('/:id', intersectionControllers.getIntersection);
+router.put('/mode-control/:id', intersectionControllers.updateModeControl);
 router.get('/get-data/:id', intersectionControllers.getData);
 
 module.exports = router
