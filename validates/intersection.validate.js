315752dbfddadfc4fbc33f47974c62b9c46c7836
= <=>
2020-04-17 11:32:54 +0700

    deleted checkField

--- validates/intersection.validate.js
+++ validates/intersection.validate.js
@@ -1,28 +1,6 @@
 var intersectionModel = require('../models/intersection.model');
 
 module.exports = {
-    checkField: function (req, res, next) {
-        var errors = [];
-        if (!req.body.name) 
-            errors.push('Name is required!');
-        if (!req.body.coordinates) 
-            errors.push('Coordinates is required!');
-        if (!req.body.bearing) 
-            errors.push('Bearing is required!');
-        if (!req.body.trafficLights) 
-            errors.push('Traffic lights is required!');
-        if (errors.length) {
-            /* return res.render('', {
-                errors: errors,
-                values: req.body
-            }); */
-            return res
-            .status(400)
-            .json(errors)
-        }
-        next();
-    },
-
     nameExists: function (req, res, next) {
         intersectionModel
         .findOne({name: req.body.name})
@@ -39,8 +17,7 @@ module.exports = {
         .catch(function(error) {
             return res
             .status(501)
-            .json(error)
+            .json({ message: 'Error!' })
         })
     },
-
 }
