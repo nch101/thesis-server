1dc691bbad8ee3a24a7058b79cea667cd5482b25
= <=>
2020-05-07 11:23:30 +0700

    add location

--- models/traffic-light.model.js
+++ models/traffic-light.model.js
@@ -1,6 +1,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var pointSchema = new Schema({
+    type: {
+        type: String,
+        enum: ['Point'],
+        require: true
+    },
+    coordinates: {
+        type: [Number],
+        required: true
+    }
+});
+
 var trafficLightSchema = new Schema({
     intersection: {
         type: Schema.Types.ObjectId, 
@@ -10,6 +22,10 @@ var trafficLightSchema = new Schema({
         type: String,
         required: true
     },
+    location: [{
+        type: pointSchema,
+        required: true
+    }],
     timeRed: {
         type: Number,
         required: true
@@ -23,10 +39,6 @@ var trafficLightSchema = new Schema({
         required: true
     },
     camip: String,
-    bearing: {
-        type: Number,
-        required: true
-    }
 });
 
 module.exports = mongoose.model('Traffic-light', trafficLightSchema);
