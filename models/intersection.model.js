7222232a44fdd98dab92a5e73c0b0c86c73bcef1
= <=>
2020-05-07 11:23:11 +0700

    remove location

--- models/intersection.model.js
+++ models/intersection.model.js
@@ -1,32 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var pointSchema = new Schema({
-    type: {
-        type: String,
-        enum: ['Point'],
-        require: true
-    },
-    coordinates: {
-        type: [Number],
-        required: true
-    }
-});
-
 var intersectionSchema = new Schema({
-    name: {
+    intersectionName: {
         type: String,
         required: true,
         uniqueCaseInsensitive: true
     },
-    location: [{
-        type: pointSchema,
-        required: true
-    }],
-    bearings: [{
-        type: Number,
-        required: true
-    }],
     modeControl: {
         type: String,
         enum: ['automatic', 'manual', 'emergency'],
