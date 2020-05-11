481b3229a5574ec6e55cdce8358a4dd827844213
= <=>
2020-05-11 17:45:57 +0700

    add location field

--- models/intersection.model.js
+++ models/intersection.model.js
@@ -1,11 +1,28 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var pointSchema = new Schema({
+    type: {
+        type: String,
+        enum: ['Point'],
+        require: true
+    },
+    coordinates: {
+        type: [Number],
+        required: true
+    }
+});
+
 var intersectionSchema = new Schema({
     intersectionName: {
         type: String,
+        lowercase: true,
         required: true,
-        uniqueCaseInsensitive: true
+        unique: true
+    },
+    location: {
+        type: pointSchema,
+        required: true
     },
     modeControl: {
         type: String,
