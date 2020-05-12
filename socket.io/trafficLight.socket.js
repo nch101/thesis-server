ad9ea39e4d8a3b2733eb4f1be9b95dbf5317429e
= <=>
2020-05-12 20:25:02 +0700

    update time light path

--- socket.io/trafficLight.socket.js
+++ socket.io/trafficLight.socket.js
@@ -1,10 +1,30 @@
+var intersectionModel = require('../models/intersection.model');
+
 module.exports = function(io) {
-	const trafficLightPath = io.of('/data-collect');
-	trafficLightPath.on('connect', function (socket) {
-		socket.emit('change-light', 'change');
-		socket.on('change-light', function (data) {
-			console.log(data);
+	const timeLight = io.of('/socket/time-light');
+	const controlLightPath = io.of('/socket/control-light');
+	
+	controlLightPath.on('connect', function(socket) {
+		socket.on('[center]-change-light', function(data) {
+			socket.broadcast.emit('[intersection]-change-light', data)			
 		});
-		trafficLightPath.emit('hi', 'Success!');
+		socket.on('disconnect', function(reason) {
+			console.log('Reason of disconnection: ' + reason);
+		})
 	});
+
+	timeLight.on('connect', function(socket) {
+		socket.on('[intersection]-time-red', function(timeRed) {
+			socket.broadcast.emit('[center]-time-red', timeRed);
+		})
+		socket.on('[intersection]-time-yellow', function(timeRed) {
+			socket.broadcast.emit('[center]-time-yellow', timeRed);
+		})
+		socket.on('[intersection]-time-green', function(timeRed) {
+			socket.broadcast.emit('[center]-time-green', timeRed);
+		})
+		socket.on('disconnect', function(reason) {
+			console.log('Reason of disconnection: ' + reason);
+		})
+	})
 }
