06839d36c3bccb44351aa34cef19cf1e99e7d699
= <=>
2020-05-14 22:04:55 +0700

    update socket io and animation traffic light

--- public/javascripts/intersection.control.js
+++ public/javascripts/intersection.control.js
@@ -16,6 +16,10 @@ var rightStreetTime = document.getElementById('right-street-time')
 var bottomStreetTime = document.getElementById('bottom-street-time')
 var leftStreetTime = document.getElementById('left-street-time')
 
+var topStreetLight = document.getElementById('top-street-light');
+var rightStreetLight = document.getElementById('right-street-light');
+var bottomStreetLight = document.getElementById('bottom-street-light');
+var leftStreetLight = document.getElementById('left-street-light')
 
 var idIntersection;
 
@@ -79,7 +83,7 @@ function renderInfoIntersection(res) {
     var modeControl = res.data.modeControl;
     var deltaTime = res.data.delta;
     var streetInfo = res.data.trafficLights;
-    var streetArray = [topStreet, rightStreet, bottomStreet, leftStreet];
+    var streetArray = [rightStreet, bottomStreet, leftStreet, topStreet];
 
     renderInteract.style.display = 'flex';
 
@@ -197,25 +201,53 @@ function automaticControl(streetInfo) {
     stateControl.innerText = 'automatic';
 }
 
-function renderTimeLight(data) {
-    
-}
-
-function renderStateLight(data) {
-
-}
-
-function controlLight(data) {
-    
-}
-
-/** Socket IO **/
 
 const stateLightSocket = io(window.location.host + '/socket/state-light');
 const controlLightSocket = io(window.location.host + '/socket/control-light');
 
-
 function getStateLight() {
     stateLightSocket.on('[center]-time-light', renderTimeLight)
-    stateLightSocket.on('[center]-time-light', renderStateLight)
+    stateLightSocket.on('[center]-light-state', renderStateLight)
 }
+getStateLight()
+
+
+function renderTimeLight(timeLight) {
+    var timeLightArray = [rightStreetTime, bottomStreetTime, leftStreetTime, topStreetTime];
+    for (var i in timeLight) {
+        timeLightArray[i].innerHTML = timeLight[i]
+    }
+
+}
+
+function renderStateLight(stateLight) {
+    var timeLightArray = [rightStreetTime, bottomStreetTime, leftStreetTime, topStreetTime];
+    var colorLightArray = [rightStreetLight, bottomStreetLight, leftStreetLight, topStreetLight];
+
+    for (var i in stateLight) {
+        if (stateLight[i] == 'red') {
+            timeLightArray[i].classList.remove('yellow-number', 'green-number');
+            timeLightArray[i].classList.add('red-number');
+            
+            colorLightArray[i].children[0].classList.add('red-light');
+            colorLightArray[i].children[1].classList.remove('yellow-light');
+        }
+        else if (stateLight[i] == 'yellow') {
+            timeLightArray[i].classList.remove('red-number', 'green-number');
+            timeLightArray[i].classList.add('yellow-number');
+
+            colorLightArray[i].children[1].classList.add('yellow-light');
+            colorLightArray[i].children[2].classList.remove('green-light')
+        }
+        else if (stateLight[i] == 'green') {
+            timeLightArray[i].classList.remove('yellow-number', 'red-number');
+            timeLightArray[i].classList.add('green-number');
+
+            colorLightArray[i].children[0].classList.remove('red-light');
+            colorLightArray[i].children[2].classList.add('green-light')
+        }
+    }
+}
+
+/** Socket IO **/
+
--- socket.io/trafficLight.socket.js
+++ socket.io/trafficLight.socket.js
@@ -1,27 +1,33 @@
 var intersectionModel = require('../models/intersection.model');
 
 module.exports = function(io) {
-	const stateLight = io.of('/socket/state-light');
+	const stateLightPath = io.of('/socket/state-light');
 	const controlLightPath = io.of('/socket/control-light');
 	
 	controlLightPath.on('connect', function(socket) {
 		socket.on('[center]-change-light', function(data) {
-			socket.emit('[intersection]-change-light', data)			
+			controlLightPath.emit('[intersection]-change-light', data)			
 		});
+		socket.on('[center]-change-mode', function(data) {
+			controlLightPath.emit('[intersection]-change-mode', data)
+		})
 		socket.on('disconnect', function(reason) {
 			console.log('Reason of disconnection: ' + reason);
 		})
 	});
 
-	stateLight.on('connect', function(socket) {
+	stateLightPath.on('connect', function(socket) {
 		socket.on('[intersection]-time-light', function(timeLight) {
-			socket.emit('[center]-time-light', timeLight);
+			stateLightPath.emit('[center]-time-light', timeLight);
+			console.log(timeLight);
 		})
 		socket.on('[intersection]-light-state', function(lightState) {
-			socket.emit('[center]-light-state', lightState);
+			stateLightPath.emit('[center]-light-state', lightState);
+			console.log(lightState);
 		})
 		socket.on('disconnect', function(reason) {
 			console.log('Reason of disconnection: ' + reason);
 		})
 	})
+	
 }
