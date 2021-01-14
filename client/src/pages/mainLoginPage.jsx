a74513c7805e60c899c25df0671663f79da8f8f9
nch101 <huynguyencong98@gmail.com>
2021-01-14 19:38:46 +0700

    create client page

---
+++ client/src/pages/mainLoginPage.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const mainLoginPage = (
+    <div className='page-wrapper'>
+        <div className='page-container'>
+            <div className='login-box'>
+                <div className=''>
+
+                </div>
+                <p className='login-title'> he thong den giao thong thong minh </p>
+                <div className=''>
+                    <p className='option-title'> Chon kieu dang nhap </p>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+
+export default mainLoginPage;
